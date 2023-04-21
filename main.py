aa7c257a3b7345606ca2697ea4088db7e7e395a6
Gabriel Medina <medina.rj@live.com>
2023-04-21 18:55:14 -0300

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mainCEP import buscadorEndereco
 
 opc = "0"
 while opc != "3":
-    opc = input("### MENU PRINCIPAL ###\n(1) Validação de CPF/CNPJ\n(2) Busca de Endereços\n(3) SAIR\nEscolha a validação:")
+    opc = input("### MENU PRINCIPAL ###\n(1) Validação de CPF/CNPJ\n(2) Busca de Endereços\n(3) SAIR\nEscolha a opção:")
     if opc == "1":
         mainCPFCNPJ()
     elif opc == "2":
